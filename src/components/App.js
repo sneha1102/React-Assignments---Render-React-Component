c5975596914f0a37dfc757ff930cc4b4a7a94e64
sneha1102 <36694331+sneha1102@users.noreply.github.com>
2020-11-07 12:14:31 +0530

    changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,12 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
-
+import React, { Component, useState } from "react";
+import "../styles/App.css";
+import Comp from "./Comp";
 const App = () => {
   return (
-    <></>
-  )
-}
-
+    <div id="root">
+      <Comp />
+    </div>
+  );
+};
 
 export default App;
---
+++ src/components/Comp.js
@@ -0,0 +1,12 @@
+import React, { Component, useState } from "react";
+import "../styles/App.css";
+
+const Comp = () => {
+  return (
+    <>
+      <p>I am learning React. My life is getting better. </p>
+    </>
+  );
+};
+
+export default Comp;
